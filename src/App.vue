c48398cd88775df735a6bc9dc5989330ca8f1901
dmarcs00 <dmarcs00@estudiantes.unileon.es>
2021-06-18 18:50:53 +0200

    Añadidos capítulos vistos al seleccionar una serie
    Ahora se muestran que capítulos han sido vistos y cuales no independientemente de la serie que se seleccione de las listas

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar
+        elevate-on-scroll
+        scroll-target="#scrolling"
+        dark
+      >
+        <v-toolbar-title class=" display-1">Polaflix</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <h2 class=" display-2">Bienvenido Usuario</h2>
+        <v-spacer></v-spacer>
+         <template 
+          v-slot:extension>
+        <v-tabs
+         align-with-title
+         slider-size = "4"
+         >
+          <v-tabs-slider
+           color="teal"
+           />
+          <v-tab to="/">Inicio</v-tab>
+          <v-tab to="/about">Agregar Serie</v-tab>
+          <v-tab to="/facturas">Facturas</v-tab>
+          <v-tab v-show="false" to="/verSerie"></v-tab>
+        </v-tabs>
+      </template>
+      </v-app-bar>
+      <router-view v-bind="myProps"></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  tab: null,
+  
+  data: () => ({
+    usuario: null,
+  }),
+  created(){
+      var self = this;
+      this.axios.get("http://localhost:8080/api/usuarios/usr1").then((result) => {  
+         self.usuario = result.data;
+      });
+  },
+  computed:{
+    myProps() {
+      if (this.$route.name === 'Principal') { 
+        let prop = this.usuario;
+        return { prop: prop }
+        }
+      if(this.$route.name ==='Facturas'){
+        let prop=[
+          {
+            facturas: this.usuario.facturas,
+            vip:this.usuario.es_VIP
+          }
+        ]
+         return { prop: prop }
+        }
+      else { return null}
+    }
+  }
+ 
+};
+</script>
+
+<style>
+.scale-enter-active,
+.scale-leave-active {
+  transition: all 0.5s ease;
+}
+
+
+.scale-enter-from,
+.scale-leave-to {
+  opacity: 0;
+  transform: scale(0.9);
+}
+</style>
---
+++ src/components/Facturas.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-container>
+    <v-sheet
+        class="mx-auto"
+        max-width="900"
+    >
+        <v-slide-group
+            center-active
+            show-arrows
+            v-model="model"
+        >
+        <v-slide-item
+            v-for="n in modelMes+1"
+            :key="n"
+            v-slot="{ active, toggle }"
+        >
+            <v-btn
+            class="mx-2"
+            :input-value="active"
+            active-class="teal white--text"
+            depressed
+            
+            @click="toggle"
+            >
+            {{meses[n-1]}} 2021
+            </v-btn>
+        </v-slide-item>
+        </v-slide-group>
+    </v-sheet>
+
+    <v-data-table
+        :headers="headers"
+        :items="facturasAux"
+        class="elevation-1 mt-4"
+        hide-default-footer
+    >
+    <template slot="items" slot-scope="props">
+        <td class="text-xs-right">{{ props.item.fecha }}</td>
+        <td class="text-xs-right">{{ props.item.nombre_serie }}</td>
+        <td class="text-xs-right">{{ props.item.temporada_x_capitulo }}</td>
+        <td class="text-xs-right">{{ props.item.precio }}</td>
+        </template>
+    </v-data-table>
+    <div class="mr-2 mt-4" style="text-align: right;">
+        <v-btn flat dark color="teal" x-large >Precio: {{importe}}€ {{model}}</v-btn> 
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+      props: {
+        prop:{
+            type: Object,
+        } 
+      },
+      name:'Factura',
+      data: () => ({
+        facturas: null,
+        model: 5,
+        meses:['Enero','Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        headers: [
+            { text: 'Fecha', value: 'fecha' },
+            { text: 'Serie', value: 'nombre_serie' },
+            { text: 'Episodio', value: 'temporada_x_capitulo' },
+            { text: 'Precio (€)', value: 'precio' },
+            ],
+        facturasAux: [],
+    }),
+    mounted(){
+        //Hay que leer el numero de factura para saber si en el mes actual hay que meter datos
+        this.facturas = this.prop[0].facturas;
+        this.facturas.forEach(element => {
+            var mes = element.num_factura.split("");
+            if(parseInt(mes[1],10) == this.model+1){ //para enseñar solo las facturas del mes seleccionado
+                
+                this.facturasAux = element.capitulos_vistos_factura;
+            }
+        })
+    },
+    watch:{
+        model:function(){
+             this.facturasAux = [];
+            this.facturas.forEach(element => {
+                var mes = element.num_factura.split("");
+                if(parseInt(mes[1],10) == this.model+1){ //para enseñar solo las facturas del mes seleccionado    
+                    this.facturasAux = element.capitulos_vistos_factura;
+                }
+            })
+        }
+    },
+    computed:{
+      importe: function(){
+        let sumaImportes = 0;
+        if(!this.es_vip){
+            this.facturasAux.forEach(element => {
+                sumaImportes += element.precio;
+            });
+        }else{
+            sumaImportes = 20;
+        }
+        return sumaImportes;
+      },
+      es_vip: function () {
+          return this.prop[0].vip;
+      },
+      modelMes: function(){
+          var mes = new Date();
+          return mes.getMonth(); // para pasar el mes a la bbdd (getFacturasMes) es +1 ya que con mayo devuelve 4...
+      }
+    },
+  }
+</script>
